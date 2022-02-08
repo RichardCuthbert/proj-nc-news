b6b7ee0567c2f2d9da02879a231f0a651093434f
RichardCuthbert <RichardCuthbertnc@protonmail.com>
2022-02-08 11:30:06 +0000

    Make header a link to route to the home page

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,12 @@
 import styles from "./Header.module.css";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
     <div className={styles.container}>
-      <h1>NC News</h1>
+      <Link to="/">
+        <h1>NC News</h1>
+      </Link>
       <p>login</p>
     </div>
   );
